16ac09a45bb5570b1534885abdf5dd218ac2488f
BaffledCoder <64409503+BaffledCoder@users.noreply.github.com>
2020-11-13 23:18:26 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='TOPSIS-Shubham-101803589',
+  version='0.0.1',
+  description='A very basic calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/BaffledCoder/TOPSIS-Shubham-101803589',  
+  author='Shubham Sharma',
+  author_email='ssharma5_be18@thapar.edu',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['topsis', 'thapar'], 
+  packages=find_packages(),
+  install_requires=['pandas'] 
+)
